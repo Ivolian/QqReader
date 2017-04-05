5932ad3bc8128212619f81a2c6b39bd44243b5a8
renjiajian <renjiajian@withub.net.cn>
2017-04-05 19:49:24 +0800

    add bottom tab

--- app/src/main/java/com/ivo/qqreader/main/MainAct.java
+++ app/src/main/java/com/ivo/qqreader/main/MainAct.java
@@ -9,8 +9,13 @@ import com.ivo.qqreader.main.watcher.BackPressWatcher;
 import com.ivo.qqreader.navigate.RoutePath;
 import com.ivo.qqreader.sidebar.SidebarFra;
 
+import butterknife.BindColor;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import me.majiajie.pagerbottomtabstrip.NavigationController;
+import me.majiajie.pagerbottomtabstrip.PageBottomTabLayout;
+import me.majiajie.pagerbottomtabstrip.item.BaseTabItem;
+import me.majiajie.pagerbottomtabstrip.item.NormalItemView;
 import me.yokeyword.fragmentation.SupportActivity;
 import qiu.niorgai.StatusBarCompat;
 
@@ -25,6 +30,7 @@ public class MainAct extends SupportActivity {
         ButterKnife.bind(this);
         addBackPressWatcher();
         addSidebar();
+        s();
     }
 
     private void addSidebar() {
@@ -48,6 +54,33 @@ public class MainAct extends SupportActivity {
         }
     }
 
+    @BindView(R.id.pageBottomTabLayout)
+    PageBottomTabLayout pageBottomTabLayout;
+
+    private void s() {
+        NavigationController navigationController = pageBottomTabLayout.custom()
+                .addItem(newItem(R.drawable.maintab_bookstand_icon, R.drawable.maintab_bookstand_icon_hover, "书架"))
+                .addItem(newItem(R.drawable.maintab_city_icon, R.drawable.maintab_city_icon_hover, "精选"))
+                .addItem(newItem(R.drawable.maintab_stack_icon, R.drawable.maintab_stack_icon_press, "书城"))
+                .addItem(newItem(R.drawable.maintab_category_icon, R.drawable.maintab_category_icon_hover, "发现"))
+                        .build();
+        navigationController.setSelect(0);
+        navigationController.setHasMessage(3,true);
+    }
+
+    @BindColor(R.color.colorPrimary)
+    int colorPrimary;
+
+    @BindColor(R.color.md_grey_400)
+    int md_grey_400;
+
+    private BaseTabItem newItem(int drawable, int checkedDrawable, String text) {
+        NormalItemView normalItemView = new NormalItemView(this);
+        normalItemView.initialize(drawable, checkedDrawable, text);
+        normalItemView.setTextDefaultColor(md_grey_400);
+        normalItemView.setTextCheckedColor(colorPrimary);
+        return normalItemView;
+    }
 
 
 }
