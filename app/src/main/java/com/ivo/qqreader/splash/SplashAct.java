d00dd267f37ca0a7ec7a6997ddff3e7953ae7384
renjiajian <renjiajian@withub.net.cn>
2017-04-05 12:52:14 +0800

    Act Splash

---
+++ app/src/main/java/com/ivo/qqreader/splash/SplashAct.java
@@ -0,0 +1,17 @@
+package com.ivo.qqreader.splash;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import qiu.niorgai.StatusBarCompat;
+
+public class SplashAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StatusBarCompat.translucentStatusBar(this);
+    }
+
+}
